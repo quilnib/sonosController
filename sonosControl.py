b5d65839034802663960747bda41260db0dc4cf8
Tim <timinth@gmail.com>
2017-01-20 21:02:28 +0000

    updated buttons to skip and control volume

--- sonosControl.py
+++ sonosControl.py
@@ -4,12 +4,12 @@ import RPi.GPIO as io
 io.setmode(io.BCM)
 
 skip_pin = 24
-previous_pin = 23
-nothing_pin = 22
+alt1_pin = 23
+alt2_pin = 22
 
-io.setup(skip_pin, io.IN, pull_up_down=io.PUD_DOWN)
-io.setup(previous_pin, io.IN, pull_up_down=io.PUD_DOWN)
-io.setup(nothing_pin, io.IN, pull_up_down=io.PUD_DOWN)
+io.setup(skip_pin, io.IN, pull_up_down=io.PUD_UP)
+io.setup(alt1_pin, io.IN, pull_up_down=io.PUD_UP)
+io.setup(alt2_pin, io.IN, pull_up_down=io.PUD_UP)
 sonos = None
 deviceName = "East"
 
@@ -22,23 +22,31 @@ for device in soco.discover():
         device = None        
 
 def skip(channel):
-    print "called skip"
+    print "called skip \n"
     sonos.next()
 
 def previous(channel):
-    print "called previous"
+    print "called previous \n"
     sonos.previous()
 
+def turnDown(channel):
+    print "called turnDown \n"
+    sonos.volume -= 10
+
 def turnUp(channel):
-    print "called turnUp"
+    print "called turnUp \n"
     sonos.volume += 10
     
-io.add_event_detect(skip_pin, io.RISING, callback=skip, bouncetime=300)
-io.add_event_detect(previous_pin, io.RISING, callback=previous, bouncetime=300)
-io.add_event_detect(nothing_pin, io.RISING, callback=turnUp, bouncetime=300)
+io.add_event_detect(skip_pin, io.FALLING, callback=skip, bouncetime=300)
+io.add_event_detect(alt1_pin, io.RISING, callback=turnDown, bouncetime=300)
+io.add_event_detect(alt2_pin, io.RISING, callback=turnUp, bouncetime=300)
 
 
-while True: #This needs to be called last, since it runs forever
-    time.sleep(1.0) #run for forever...
+try:
 
+    while True: #This needs to be called last, since it runs forever
+        time.sleep(1.0) #run for forever...
+
+except KeyboardInterrupt:
+        io.cleanup()
  
